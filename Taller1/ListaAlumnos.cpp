7131140560db564a18c0d6a874a7b2edf688b8b2
chomp <felipesalazar@vtr.net>
2022-09-07 10:21:17 -0300

    Inicio lista alumno

---
+++ Taller1/ListaAlumnos.cpp
@@ -0,0 +1,15 @@
+#include "ListaAlumnos.h"
+
+ListaAlumnos::ListaAlumnos()
+{
+	count = 0;
+}
+
+void ListaAlumnos::addAlumno(Alumno)
+{
+}
+
+Alumno ListaAlumnos::findAlumno()
+{
+	return Alumno();
+}
