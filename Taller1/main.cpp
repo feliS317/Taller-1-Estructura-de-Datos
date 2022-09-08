3dae17b858f94eff103021472f201827afa8f4d6
chomp <felipesalazar@vtr.net>
2022-09-08 16:45:59 -0300

    Movido el main al fondo

--- Taller1/main.cpp
+++ Taller1/main.cpp
@@ -13,85 +13,6 @@ ListaAlumnos alumnos = ListaAlumnos();
 ListaProfesores profesores = ListaProfesores();
 ListaRamos ramos = ListaRamos();
 
-bool addData(); // Las funciones deben declararse primero o colocarse el main al final - feli
-bool createNewAlumno();
-bool createNewProfesor();
-bool createNewRamo();
-
-int main()
-{
-    bool go = true;
-    while (go)
-    {
-        int option;
-        cout << "Buen dia usuario, que accion desea realizar?\n";
-        cout << "Escriba el numero que se indica al principio de la linea sin el . (punto) para realizar esa acción\n"
-            "1. Realizar una consulta\n"
-            "2. Agregar un alumno/profesor/ramo\n"
-            "3. Modificar/Eliminar dato especifico ya existente\n"
-            "4. Salir del programa\n";
-        cin >> option;
-        while (!cin.good() || option > 4)
-        {
-            cin.clear();
-            cin.ignore(numeric_limits<streamsize>::max(), '\n');
-            cout << "Valor invalido, ingrese otro valor\n";
-            cin >> option;
-        }
-        switch (option)
-        {
-        case 1:
-            break;
-        case 2:
-            if (addData())
-            {
-                cout << "Anadido correctamente\n\n";
-            }
-            else
-            {
-                cout << "No se ha podido anadir esta informacion, se ha llegado al limite de datos de esta caracteristica\n\n";
-            }
-            break;
-        case 3:
-            break;
-        case 4:
-            go = false;
-            break;
-        }
-    }
-    return 0;
-}
-
-bool addData()
-{
-    int option;
-    cout << "\nQue desea agregar?\n"
-        "1. Un alumno\n"
-        "2. Un profesor\n"
-        "3. Un ramo\n";
-    cin >> option;
-    while (!cin.good() || option > 3)
-    {
-        cin.clear();
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
-        cout << "Valor invalido, ingrese otro valor\n";
-        cin >> option;
-    }
-    switch (option)
-    {
-    case 1:
-        if (createNewAlumno()) { return true; }
-        break;
-    case 2:
-        if (createNewProfesor()) { return true; }
-        break;
-    case 3:
-        if (createNewRamo()) { return true; }
-        break;
-    }
-    return false;
-}
-
 bool createNewAlumno()
 {
     string nombre;
@@ -161,4 +82,78 @@ bool createNewRamo()
         return true;
     }
     return false;
+}
+
+bool addData()
+{
+    int option;
+    cout << "\nQue desea agregar?\n"
+        "1. Un alumno\n"
+        "2. Un profesor\n"
+        "3. Un ramo\n";
+    cin >> option;
+    while (!cin.good() || option > 3)
+    {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout << "Valor invalido, ingrese otro valor\n";
+        cin >> option;
+    }
+    switch (option)
+    {
+    case 1:
+        if (createNewAlumno()) { return true; }
+        break;
+    case 2:
+        if (createNewProfesor()) { return true; }
+        break;
+    case 3:
+        if (createNewRamo()) { return true; }
+        break;
+    }
+    return false;
+}
+
+int main()
+{
+    bool go = true;
+    while (go)
+    {
+        int option;
+        cout << "Buen dia usuario, que accion desea realizar?\n";
+        cout << "Escriba el numero que se indica al principio de la linea sin el . (punto) para realizar esa acción\n"
+            "1. Realizar una consulta\n"
+            "2. Agregar un alumno/profesor/ramo\n"
+            "3. Modificar/Eliminar dato especifico ya existente\n"
+            "4. Salir del programa\n";
+        cin >> option;
+        while (!cin.good() || option > 4)
+        {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << "Valor invalido, ingrese otro valor\n";
+            cin >> option;
+        }
+        switch (option)
+        {
+        case 1:
+            break;
+        case 2:
+            if (addData())
+            {
+                cout << "Anadido correctamente\n\n";
+            }
+            else
+            {
+                cout << "No se ha podido anadir esta informacion, se ha llegado al limite de datos de esta caracteristica\n\n";
+            }
+            break;
+        case 3:
+            break;
+        case 4:
+            go = false;
+            break;
+        }
+    }
+    return 0;
 }
